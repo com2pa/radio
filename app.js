aa0872cbd60fb42fa20e6f2660f705fd73855607
merwil vegas <com2pa@gmail.com>
2025-09-15 21:28:14 -0400

    se añadio la ruta usuario en desarrollo

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 require('dotenv').config()
-const express =require('express')
+const express = require('express')
 const app = express()
-const cors =require('cors')
+const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const morgan =require("morgan")
+const morgan = require("morgan")
+// const { graphqlHTTP } = require('graphql-http')
+const schema = require('./graphql/schema')
+const root = require('./graphql/resolvers')
 
-// 
+
+// verificar la conexión a la base de datos
+const { testConnection } = require('./db/index')
+const useRouter = require('./controllers/user')
+testConnection()
+
+
+
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
@@ -13,5 +23,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'))
 
 //rutas 
+// app.use('/graphql', graphqlHTTP({
+//   schema: schema,
+//   rootValue: root,
+//   graphiql: true // Habilita la interfaz web para probar consultas
+// }))
+
+app.use('/api/register',useRouter)
+
 
 module.exports = app
